9e51462a83c2828844e3f7d92e14f52204087487
Danielle B <danibaxx@hotmail.com>
2020-01-12 14:26:48 -0700

    brought in db-config.js, added database methods

---
+++ schemes/scheme-model.js
@@ -0,0 +1,59 @@
+const db = require('../data/db-config');
+
+function find() {
+  return db('schemes').select()
+};
+
+function findById(id) {
+  return db('schemes')
+    .where({ id })
+    .first()
+};
+
+function findSteps(scheme_id) {
+  return db('schemes')
+    .join(
+      'steps', 
+      'steps.id', 
+      'steps.scheme_id'
+    )
+    .where({ scheme_id })
+    .select(
+      'steps.scheme_id', 
+      'steps.step_number', 
+      'steps.instructions', 
+      'schemes.scheme_name'
+    )
+};
+
+async function add(data) {
+  const [id] = await db('schemes')
+    .insert(data)
+  return db('schemes')
+    .where({ id })
+    .first()
+};
+
+async function update(id, body) {
+  await db('schemes')
+    .where({ id })
+    .update(body)
+
+  return findById(id)
+};
+
+function remove() {
+  return db('schemes')
+    .where({ id })
+    .del()
+};
+
+
+module.exports = {
+  find,
+  findById,
+  findSteps,
+  add,
+  update,
+  remove,
+}
